0dc794946ec81ea7c6d75c211570a1d0d7e2b695
Craig <chasellau98@gmail.com>
2026-05-03 16:30:36 +1000

    Add padding to mobile size

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
 // components
-import Banner from './components/Banner';
-import Header from './components/Header';
-import Nav from './components/Nav';
-import About from './components/About';
-import Experience from './components/Experience';
-import Travel from'./components/Travel';
-import Contact from './components/Contact';
+import Home from './pages/home';
 
 const App = () => {
   return (
-   <div className='bg-site bg-no-repeat bg-cover overflow-hidden flex flex-col gap-y-20'>
-      <Header />
-      <Banner />
-      <Nav />
-      <About />
-      <Experience />
-      <Travel />
-      <Contact />
-   
+   <div className='px-2 md:px-0 bg-site bg-no-repeat bg-cover overflow-hidden flex flex-col gap-y-20'>
+      <Home />
     </div>
   );
 };
---
+++ src/pages/home/index.js
@@ -0,0 +1,23 @@
+import Header from '../../components/Header';
+import Banner from '../../components/Banner';
+import Nav from '../../components/Nav';
+import About from '../../components/About';
+import Experience from '../../components/Experience';
+import Travel from '../../components/Travel';
+import Contact from '../../components/Contact';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <Nav />
+      <About />
+      <Experience />
+      <Travel />
+      <Contact />
+    </div>
+  );
+};
+
+export default Home;
